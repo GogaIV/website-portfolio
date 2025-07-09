9894d61b3ca6afddfea8a35938fc08105526d6e0
Jauhar <jauhargoga@gmail.com>
2025-07-10 09:12:28 +1200

    attempted transitions sadly it is new to nextjs currently maybe in future inshallah

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+// I am infact not using transitions till nextjs updates and theres more documentation available
+// import { unstable_ViewTransition as ViewTransition } from 'react'
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,12 +27,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
   );
 }
