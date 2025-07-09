9ff7b50cd753dcef3dd63107105beb23ef2deb65
Jauhar <jauhargoga@gmail.com>
2025-07-09 18:17:39 +1200

    working as expected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@
 import { FaceIcon, ImageIcon, SunIcon } from "@radix-ui/react-icons";
 import { Button } from "@/components/ui/button";
 
-// Testing git commit as moved to new pc
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)], bg-[#2e2c2c] ">
