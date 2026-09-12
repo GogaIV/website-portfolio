360c801de62bc739f95d67217d97191fe65c595a
Jauhar Goga <jauhargoga@gmail.com>
2026-09-13 04:58:34 +1200

    fix: depenedencies

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    viewTransition: true,
-  },
 };
 
 export default nextConfig;
+
+import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
